67fc2ff4026e357781f17af6a3c0481d71eec6d5
Alejandro Aristizabal <aaristizaball@gmail.com>
2013-03-07 10:21:26 -0500

    heroku

---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+#AAL
+require 'capistrano-deploy'
+use_recipes :git, :bundle, :rails
+
+server 'tac-tus.com', :web, :app, :db, :primary => true
+set :user, 'tac_tus'
+set :deploy_to, '/iirestaurant.tac-tus.com/'
+set :repository, 'git@github.com:aaristizaball/restarurant.git'
+
+after 'deploy:update', 'bundle:install'
+
+#fin AAL
+
+set :application, "set your application name here"
+set :repository,  "set your repository location here"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+role :app, "your app-server here"                          # This may be the same as your `Web` server
+role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+
